f8e61a5bccf1428facf4836dd931bb947891c70d
jift77 <serviciotecnicotoyota@gmail.com>
2018-02-27 10:15:58 -0500

    first commit

---
+++ ExampleApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using ExampleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRepository repo;
+
+        public HomeController(IRepository repoImpl) {
+            repo = repoImpl;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(repo.Products);
+        }
+    }
+}
---
+++ ExampleApp/Infraestructure/NinjectResolver.cs
@@ -0,0 +1,53 @@
+﻿using ExampleApp.Models;
+using Ninject;
+using Ninject.Extensions.ChildKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+
+namespace ExampleApp.Infraestructure
+{
+    public class NinjectResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectResolver() : this (new StandardKernel()) { }
+
+        public NinjectResolver(IKernel ninjectKernel, bool scope = false) {
+            kernel = ninjectKernel;
+            if (!scope)
+                AddBindings(kernel);
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectResolver(AddRequestBindings(new ChildKernel(kernel)), true);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings(IKernel kernel) {
+
+        }
+
+        private IKernel AddRequestBindings(IKernel kernel) {
+            kernel.Bind<IRepository>().To<Repository>().InSingletonScope();
+            return kernel;
+        }
+    }
+}
